052aed7195e314a08f2473340f143db53bd35af6
xenqzhu <129019820+xenqzhu@users.noreply.github.com>
2023-09-30 11:14:04 +0000

    [Introduction & Instructions] Updated> Completed final code, cleaned it up and stuff ye. :P

--- introduction_instructions.py
+++ introduction_instructions.py
@@ -3,25 +3,27 @@
 def yes_no (question):
   valid = False 
   while not valid:
-    response = input(question).lower()
-      
-    # [>] YES / NO / MAYBE INPUT LOOP:
-    if response == "yes" or response == "y" or response == "yef":
-      response = "yes"
-      return response
-
-    elif response == "no" or response == "n" or response == "neh":
-      response = "no"
-      return response 
-
-    else:
-      print("<ERROR>: Please enter either 'YES' or 'NO'\n")
-
+      response = input(question).lower()
+        
+      # [>] YES / NO / MAYBE INPUT LOOP:
+      if response == "yes" or response == "y":
+        print("[CONTINUE TO PROGRAM]\n")
+       
+      elif response == "no" or response == "n":
+        print("[DISPLAY INSTRUCTIONS]\n")
+    
+      else:
+        print("<ERROR>: Please enter either 'YES' or 'NO'\n")
   
-    # < -- MAIN ROUTINE -- >
+  
+# < -- MAIN ROUTINE -- >
 
 # [>] PROGRAM INTRODUCTION:
 print("Welcome to quizith :P \n")
 
 # [>] ASK USER INPUT: 'HAVE YOU PLAYED BEFORE?'
-played = input("Have you participated in this quiz before?: ").lower()
+played = yes_no ("Have you participated in this quiz before?: ").lower()
+
+
+
+
