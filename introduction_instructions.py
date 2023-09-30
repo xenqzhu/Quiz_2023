b082ab52af92eb51f5e40ff50a3139df247069bb
xenqzhu <129019820+xenqzhu@users.noreply.github.com>
2023-09-30 11:00:42 +0000

    [Introduction & Instructions]: Updated> added 'response' function, updated user responses and loop functions. cleaned up code. :)

--- introduction_instructions.py
+++ introduction_instructions.py
@@ -1,26 +1,27 @@
 # < -- FUNCTIONS -- >
 
-# [>] PROGRAM INTRODUCTION:
-print("Welcome to quizith :P \n")
-
-valid = False 
-while not valid:
-    
-    
-    # [>] ASK USER INPUT: 'HAVE YOU PLAYED BEFORE?'
-    played = input("Have you participated in this quiz before?: ").lower()
-    
-    
+def yes_no (question):
+  valid = False 
+  while not valid:
+    response = input(question).lower()
+      
     # [>] YES / NO / MAYBE INPUT LOOP:
-    if played == "yes":
-      print("[PROGRAM CONTINUES]\n")
-  
-    elif played == "no":
-      print("[DISPLAY INSTRUCTIONS]\n")
-  
+    if response == "yes" or response == "y" or response == "yef":
+      response = "yes"
+      return response
+
+    elif response == "no" or response == "n" or response == "neh":
+      response = "no"
+      return response 
+
     else:
       print("<ERROR>: Please enter either 'YES' or 'NO'\n")
 
-    
+  
     # < -- MAIN ROUTINE -- >
 
+# [>] PROGRAM INTRODUCTION:
+print("Welcome to quizith :P \n")
+
+# [>] ASK USER INPUT: 'HAVE YOU PLAYED BEFORE?'
+played = input("Have you participated in this quiz before?: ").lower()
