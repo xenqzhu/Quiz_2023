d6d89c5d03dfb007426864f2b435145c1bbfb04c
xenqzhu <129019820+xenqzhu@users.noreply.github.com>
2023-10-12 08:48:55 +0000

    [Introduction & Instructions] Updated> added 'continue to quiz button' after displaying instructions.

--- introduction_instructions.py
+++ introduction_instructions.py
@@ -1,29 +1,43 @@
 # < -- FUNCTIONS -- >
 
-def yes_no (question):
+# [>] STARTER INTRODUCTION: (YES / NO):
+
+def yes_no(question):
   valid = False 
   while not valid:
-      response = input(question).lower()
+    response = input(question).lower()
+    if response == "yes" or response == "y":
+      return response
+      
+    elif response == "no" or response == "n":
+      return response
+      
+    else:
+      print("<ERROR>: Please enter either 'YES' or 'NO'\n")
+
         
-      # [>] YES / NO / MAYBE INPUT LOOP:
-      if response == "yes" or response == "y":
-        print("[CONTINUE TO PROGRAM]\n")
-       
-      elif response == "no" or response == "n":
-        print("[DISPLAY INSTRUCTIONS]\n")
-    
-      else:
-        print("<ERROR>: Please enter either 'YES' or 'NO'\n")
-  
-  
+# [>] INSTRUCTIONS LAYOUT:
+
+def instructions ():
+  print()
+  print("    <-----| HOW TO USE |----->\n")
+  print(" | WELCOME TO THE TIMES-TABLE QUIZ |\n")
+  print(""" • You will select a times-table you would like to be tested on (MIN - MAX: 1x-12x) 
+ • MAX questions: 50
+ • INFO SERVICE \n""")
+  return""
+
+
 # < -- MAIN ROUTINE -- >
 
-# [>] PROGRAM INTRODUCTION:
+# [>] WELCOME INTRODUCTION:
 print("Welcome to quizith :P \n")
 
-# [>] ASK USER INPUT: 'HAVE YOU PLAYED BEFORE?'
+# [>] ASK USER INPUT: 'HAVE YOU PLAYED BEFORE?':
 played = yes_no ("Have you participated in this quiz before?: ").lower()
 
+if played == "no" or "n":
+  instructions()
 
-
-
+# [>] CONTINUE TO QUIZ:
+  input("> CONTINUE TO QUIZ: <PRESS ENTER>")
