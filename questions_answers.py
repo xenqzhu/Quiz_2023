a38ab7ecfc4a49fe470b9a7185ccfcedba5968a5
xenqzhu <129019820+xenqzhu@users.noreply.github.com>
2023-10-29 07:58:37 +0000

    [Questitons_Answers] Updated: Added 'num_check' and 'times' variable  |  User can now input own times-table  |  '<ERROR>' code also inputted.

--- questions_answers.py
+++ questions_answers.py
@@ -1,17 +1,26 @@
 # < -- FUNCTIONS -- >
 
-from random import randint
+def num_check(question):
+  error = "<ERROR> Invalid input. Please enter a whole number. "
+  try:
+    response = int(input(question))
+    return response
+  except ValueError: 
+    print(error)
 
-while True:
-  num1 = randint(2,9)
-  num2 = randint(2,9)   
-  product = num1 * num2
-  response = int(input(f"{num1} x {num2}?: "))
-  if not response:
-    break
- 
-  ans = int(response)
-  print('Correct! :D \n' if ans == product else 'Incorrect...:C \n')
+times = num_check("Enter a Times-Table: ")
+print()
 
-  # < -- MAIN ROUTINE -- >
+for x in range (1,20):
+  answer = times * x
+  guess = num_check ("What is {} x {}? = ".format(times,x))
+
+  if guess == answer:
+    print("Correct! :D \n")
+  else: 
+    print("Incorrect :C \n")
+
+
+
+# < -- MAIN ROUTINE -- >
 
